d6c52169955d79e0a3ac2972ab5779b14d7fa70c
srey-a <65911212+srey-a@users.noreply.github.com>
2020-07-14 21:20:48 +0530

    Update CalculatorProject.py

--- CalculatorProject.py
+++ CalculatorProject.py
@@ -1,15 +1,1 @@
-#Calculatorproject using Tkinder
 
-#import everything from tkinder module
-
-from tkinter import *
-
-expression=""  #globally declare the expression variable
-
-def press(num):   #in the text entry box
-
-    global expression
-
-    expression=expression+str(num)   #concatenation of the string
-
-    equation.set(expression)   #function to evaluate by using set method
