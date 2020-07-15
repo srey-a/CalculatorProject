fddbf84b95b84307aa969cc54248fca107a84367
srey-a <65911212+srey-a@users.noreply.github.com>
2020-07-15 12:43:16 +0530

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -1,1 +1,20 @@
+from tkinter import *
+
+window=Tk()
+window.geometry("280x400")
+window.title("CALCULATOR")
+window.configure(bg="#669999")
+
+def hello():
+    print("button clicked")
+
+button1=Button(window,text="ok",command=hello)
+button2=Button(window,text="ok",command=hello)
+
+button1.grid(row=0,column=0)
+button2.grid(row=1,column=0)
+
+
+
+window.mainloop()
 
